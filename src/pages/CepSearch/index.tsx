95a2dc108255fceaedf94a7aeb2f1a841553b201
Tiago Ssa <tiago.silva301@gmail.com>
2021-12-27 13:20:39 +0000

    handleChange and handleSubmit

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,19 +1,54 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+
+type FormData = {
+  postcode: string;
+  test: string;
+}
 
 const CepSearch = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    postcode: '',
+    test: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({...formData, [name]:value });
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Find or check a Postcode</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
+              name="postcode"
+              value={formData.postcode}
               className="search-input"
               placeholder="PostCode (Enter only number)"
-              onChange={() => {}}
+              onChange={handleChange}
+            />
+             <input
+              type="text"
+              name="test"
+              value={formData.test}
+              className="search-input"
+              placeholder="TEST"
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Search
